b9f719be3144ad29c27b570cd3c2ddd6e911483a
Kyle L. Jensen <kljensen@gmail.com>
2014-06-24 13:50:44 -0400

    Initial import

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tunatankApp
+ * @description
+ * # tunatankApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tunatankApp', []);
